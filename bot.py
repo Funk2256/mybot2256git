76a0646d81645ab2f64464a7e9535c9656e3da46
Funk2256 <funk2256@gmail.com>
2019-04-17 16:49:53 +0700

    ‘V1’

---
+++ bot.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import config
+import telebot
+from telebot import types
+from telebot import apihelper
+
+apihelper.proxy = {'https':'socks5://userid10Oh:mlLbfK@185.36.191.39:7992'}
+
+bot = telebot.TeleBot(config.token)
+
+# Обработчик команд '/start' и '/help'.
+@bot.message_handler(commands=['start', 'help', 'keyboard'])
+def handle_start_help(message):
+    pass
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+
+"""markup = types.ReplyKeyboardMarkup()
+markup.row('a', 'v')
+markup.row('c', 'd', 'e')
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+	bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+"""
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+token = '762303185:AAEJuj85I5-4DIvuAnWVF_j4cP1Zrxm8DVo'
