1399e174c682be4e7eeb9b44721282403b464f27
Funk2256 <funk2256@gmail.com>
2019-04-21 18:00:07 +0700

    init

--- bot.py
+++ bot.py
@@ -4,7 +4,10 @@ import telebot
 from telebot import types
 from telebot import apihelper
 import os
-import flask
+from flask import Flask
+
+
+app = Flask(__name__)
 
 apihelper.proxy = {'https':'socks5://userid10Oh:mlLbfK@185.36.191.39:7992'}
 
@@ -28,25 +31,10 @@ markup.row('c', 'd', 'e')
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
 	bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
 """
-if __name__ == '__main__':
-     bot.polling(none_stop=True)
+@app.route("/")
+def hello():
+    return "Hello from Python!"
 
-server = flask.Flask(__name__)
- 
- 
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    bot.process_new_updates([types.Update.de_json(
-         flask.request.stream.read().decode("utf-8"))])
-    return "!", 200
- 
- 
-@server.route('/', methods=["GET"])
-def index():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
-    return "Hello from Heroku!", 200
- 
- 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
