928f41ae4720b7baa2e8cc79f47a370d317ffe5b
Funk2256 <funk2256@gmail.com>
2019-04-21 18:14:45 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -4,19 +4,8 @@ import telebot
 from telebot import types
 from telebot import apihelper
 import os
-from flask import Flask
+#from flask import Flask
 
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello from Python!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-    
 apihelper.proxy = {'https':'socks5://userid10Oh:mlLbfK@185.36.191.39:7992'}
 
 bot = telebot.TeleBot(config.token)
@@ -30,6 +19,19 @@ def handle_start_help(message):
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
 
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Python!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 """markup = types.ReplyKeyboardMarkup()
 markup.row('a', 'v')
