f599d4f034732cb7f6c7032deced921884d27c9c
Funk2256 <funk2256@gmail.com>
2019-04-25 14:22:22 +0700

    v1 bot

--- bot.py
+++ bot.py
@@ -8,19 +8,16 @@ from flask import Flask
 
 
 bot = telebot.TeleBot(config.token)
-
 # Обработчик команд '/start' и '/help'.
 @bot.message_handler(commands=['start', 'help', 'keyboard'])
+
 def handle_start_help(message):
     pass
 
 @bot.message_handler(content_types=["text"])
+
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
-
-
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+	bot.send_message(message.chat.id, message.text)
 
 app = Flask(__name__)
 
