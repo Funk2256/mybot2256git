da8974c000d10d7f252bb84d9a533d93266d6726
Funk2256 <funk2256@gmail.com>
2019-05-12 15:39:00 +0700

    v 0.1

--- bot.py
+++ bot.py
@@ -1,32 +1,20 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/python3
 import config
 import proxy
 import telebot
-from telebot import types
-import os
-from flask import Flask
+import run_server
+#import certifi
+#import requests
 
+#url = 'http://api.telegram.org'
+#r = requests.get(url, verify=False)
 
 bot = telebot.TeleBot(config.token)
-# Обработчик команд '/start' и '/help'.
-@bot.message_handler(commands=['start', 'help', 'keyboard'])
-
-def handle_start_help(message):
-    pass
 
 @bot.message_handler(content_types=["text"])
-
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-	bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, message.text)
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello from Python!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
-# Все описанно в офф. документации heroku
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
--- proxy.py
+++ proxy.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from telebot import apihelper
-apihelper.proxy = {'https':'socks5://userid10Oh:mlLbfK@185.36.191.39:7992'}
+apihelper.proxy = {'https':'socks5h://tg-funk2256:7W6SiwIA@1ru.socksy.seriyps.ru:7777'}
