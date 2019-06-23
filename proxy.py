8adafd82112c1ce90f2f82d740b67a6798f45b13
Funk2256 <funk2256@gmail.com>
2019-06-23 13:32:21 +0700

    v 0.1

--- proxy.py
+++ proxy.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+
 from telebot import apihelper
-apihelper.proxy = {'https':'socks5h://tg-funk2256:7W6SiwIA@1ru.socksy.seriyps.ru:7777'}
+apihelper.proxy = {'https':'socks5h://tg-funk2256:QScZxb3Z@socksy.seriyps.ru:7777'}
